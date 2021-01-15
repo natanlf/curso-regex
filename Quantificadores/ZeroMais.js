12b2391abbd0eef2b4f7ceec404778df7bd78562
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2021-01-15 13:34:39 -0300

    Quantificador: * (Zero-Mais)

---
+++ Quantificadores/ZeroMais.js
@@ -0,0 +1,13 @@
+const texto1 = 'De longe eu avistei o fogo e uma pessoa gritando: FOGOOOOOO!'
+const texto2 = 'There is a big fog in NYC'
+
+// * -> zero ou mais
+const regex = /fogo*/gi
+
+console.log(texto1.match(regex))
+console.log(texto2.match(regex))
+/*
+[ 'fogo', 'FOGOOOOOO' ]
+[ 'fog' ]
+Pega o máximo que consegue
+*/
