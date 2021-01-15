989e93019aa83082413f457c1a08d6f82e1e0604
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2021-01-15 14:20:43 -0300

    Guloso vs Não Guloso

---
+++ Quantificadores/NaoGuloso.js
@@ -0,0 +1,22 @@
+const texto = '<div>Conteudo 01</div><div>Conteudo 02</div>'
+
+//Quantificadores SÃO gulosos (greedy)...
+console.log(texto.match(/<div>.+<\/div>/g))
+console.log(texto.match(/<div>.*<\/div>/g))
+console.log(texto.match(/<div>.{0,100}<\/div>/g))
+
+/*
+[ '<div>Conteudo 01</div><div>Conteudo 02</div>' ]
+[ '<div>Conteudo 01</div><div>Conteudo 02</div>' ]
+[ '<div>Conteudo 01</div><div>Conteudo 02</div>' ]
+*/
+
+//Quantificadores NÃO gulosos (lazy)...
+console.log(texto.match(/<div>.+?<\/div>/g))
+console.log(texto.match(/<div>.*?<\/div>/g))
+console.log(texto.match(/<div>.{0,100}?<\/div>/g))
+/*
+[ '<div>Conteudo 01</div>', '<div>Conteudo 02</div>' ]
+[ '<div>Conteudo 01</div>', '<div>Conteudo 02</div>' ]
+[ '<div>Conteudo 01</div>', '<div>Conteudo 02</div>' ]
+*/
